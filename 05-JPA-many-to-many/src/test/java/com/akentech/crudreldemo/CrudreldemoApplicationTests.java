700cc9b29c204520bbf33866eea08744938a0147
Kenrique <ngwakneri2016@gmail.com>
2023-07-24 13:23:30 +0100

    JPA many-to-mmany mapping

---
+++ 05-JPA-many-to-many/src/test/java/com/akentech/crudreldemo/CrudreldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akentech.crudreldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudreldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
